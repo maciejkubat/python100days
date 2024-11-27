85b4109ae56c645058e433c4a2ad23a4d07c7401
Maciej Kubat <maciek.kubat@gmail.com>
2024-11-27 12:56:24 +0100

    Day 16

---
+++ Day_16/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+
+my_screen = Screen()
+my_screen.setup(width=400, height=300)  # Set up the screen size
+
+
+my_screen.exitonclick()
