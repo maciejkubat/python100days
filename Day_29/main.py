104d7eea6931d025a91552059102c1f7b3fb54a0
Maciej Kubat <maciek.kubat@gmail.com>
2025-02-06 10:38:57 +0100

    Completed day 30

--- Day_29/main.py
+++ Day_29/main.py
@@ -11,9 +11,27 @@ def pass_gen():
     pyperclip.copy(generated_password)
     password_entry.insert(0,generated_password)
 
+# ---------------------------- SEARCH ------------------------------- #
+def find_password():
+    website = website_entry.get().capitalize()
+    try:
+        with open('data.json', mode="r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="Data file not found")
+    else:
+        if website in data:
+            website_data = data[website]
+            messagebox.showinfo(title=f"Password info for {website}", message=f"Email: {website_data["email"]}\nPassword: {website_data["password"]}")
+            pyperclip.copy(website_data["password"])
+        else:
+            messagebox.showerror(title="Error", message="No details for website exist")
+
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    website = website_entry.get()
+    website = website_entry.get().capitalize()
     email = email_entry.get()
     password = password_entry.get()
     new_data = {
@@ -25,14 +43,21 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Warning", message="Do not leave any fields empty")
     else:
-        with open('data.json', mode="w") as file:
-            data = json.load(file)
+        try:
+            with open('data.json', mode="r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            with open('data.json', mode="w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
             data.update(new_data)
-            json.dump(new_data, file, indent=4)
-        website_entry.delete(0,END)
-        email_entry.delete(0, END)
-        email_entry.insert(0, "maciek.kubat@gmail.com")
-        password_entry.delete(0, END)
+            with open('data.json', mode="w") as file:
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0,END)
+            email_entry.delete(0, END)
+            email_entry.insert(0, "maciek.kubat@gmail.com")
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,9 +73,12 @@ canvas.grid(column=1,row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=21)
 website_entry.focus()
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.grid(column=1, row=1, columnspan=1)
+
+search_button = Button(text="Search", command=find_password, width=13)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
